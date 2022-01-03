b3116d62e25d2e8f099d4ff7cd93d1e8a561bd92
Joseph Zhang <tweejjhi4016@163.com>
2022-01-03 09:23:08 +0800

    config files

--- res/conf/default_conf/goto.php
+++ res/conf/default_conf/goto.php
@@ -23,61 +23,14 @@
 $title=array(
         "世界上第一个网站",
         "WCC实验室官网",
-        "科大镜像源",
-        "清华大学镜像源",
-        "Base64",
-        "The Old Reader",
-        "新拟态css生成",
-        "时光序网页版",
-        "古汉语词典",
-        "日历网",
-        "标准北京时间",
-        "在线闹钟",
-        "Github HOSTS",
-        "thum.io",
-        "网易163邮箱",
-        "iis7站长之家",
-        "青岛市招生考试院",
-        "在线工具"
 ,"___END");
 $link=array(
         "http://info.cern.ch",
-        "https://wcc.group",
-        "https://mirrors.ustc.edu.cn",
-        "https://mirrors.tuna.tsinghua.edu.cn",
-        "https://base64.us",
-        "https://theoldreader.com",
-        "https://neumorphism.io",
-        "https://web.shiguangxu.com",
-        "https://wyw.hwxnet.com",
-        "https://www.rili.com.cn",
-        "https://beijing-time.org",
-        "https://naozhong.net.cn",
-        "https://git.yoqi.me/lyq/github-host/raw/master/hosts",
-        "https://www.thum.io",
-        "https://mail.163.com",
-        "http://iis7.com",
-        "http://edu.qingdao.gov.cn/n32563465/",
-        "https://tool.lu/"
+        "https://wcc.group"
 );
+/* Introductions */
 $intro=array(
         "info.cern.ch",
-        "wcc.group",
-        "mirrors.ustc.edu.cn",
-        "mirrors.tuna.tsinghua.edu.cn",
-        "Base64 在线编码解码",
-        "theoldreader.com",
-        "neumorphism.io",
-        "web.shiguangxu.com",
-        "wyw.hwxnet.com",
-        "www.rili.com.cn",
-        "beijing-time.org",
-        "naozhong.net.cn",
-        "github hosts",
-        "www.thum.io",
-        "mail.163.com",
-        "iis7.com",
-        "青岛市招生考试院官网",
-        "tool.lu"
+        "wcc.group"
 );
 ?>
